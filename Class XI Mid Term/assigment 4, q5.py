15347562287a9774a77fb02f2f6111c253bf6af0
nebula <109933732+nebulaxoxo@users.noreply.github.com>
2022-09-24 15:20:50 +0530

    revised

--- Class XI Mid Term/assigment 4, q5.py
+++ Class XI Mid Term/assigment 4, q5.py
@@ -14,16 +14,3 @@ while(i<=5):
   print(" " * (5 - i) +"*" * i)
   i+=1
 
-'''part 4'''
-k = 0
-
-for i in range(1, 6):
-    for space in range(1, (5-i)+1):
-        print(end="  ")
-   
-    while k !=(2*i-1):
-        print("* ", end="")
-        k += 1
-   
-    k = 0
-    print()
