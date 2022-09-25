542600b8ab3f28de847427563f9ef871a7fa13e8
nebula <109933732+nebulaxoxo@users.noreply.github.com>
2022-09-25 23:05:35 +0530

    Create assigment 1, q3.py

---
+++ Class XI Mid Term/assigment 1, q3.py
@@ -0,0 +1,9 @@
+name = str(input("Enter name: "))
+sub1 = int(input("Enter marks in first subject: "))
+sub2 = int(input("Enter marks in second subject: "))
+sub3 = int(input("Enter marks in third subject: "))
+sub4 = int(input("Enter marks in fourth subject: "))
+sub5 = int(input("Enter marks in fifth subject: "))
+total = sub1 + sub2 + sub3 + sub4 +sub5
+avg = total/5
+print("Total marks: ", total, "\n Average marks: ", avg)
